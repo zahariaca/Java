df3ed7b7ed31983233a67a8137939271cc46821f
zahariaca <zaharia.c.alexandru@gmail.com>
2016-11-23 17:25:12 +0200

    added random array generation to BigO project

--- BigONotations/src/com/zahariaca/bigo/Main.java
+++ BigONotations/src/com/zahariaca/bigo/Main.java
@@ -1,12 +1,23 @@
 package com.zahariaca.bigo;
 
 public class Main {
+    private int[] theArray;
+    private int arraySize = 0;
 
     public static void main(String[] args) {
-	// write your code here
+        Main bigArray = new Main(100);
+        bigArray.generateRandomArray();
+        System.out.println(bigArray.theArray[70]);
     }
 
-    public Main(int size){
-
+    public Main(int arraySize){
+        this.arraySize = arraySize;
+        theArray = new int[arraySize];
     }
+    public void generateRandomArray(){
+        for (int i = 0; i < arraySize; i++){
+            theArray[i] = (int) (Math.random() * 1000) + 10;
+        }
+    }
+
 }
