059bb62938193f609d4d652d77bddbb1bda1f1a7
zahariaca <zaharia.c.alexandru@gmail.com>
2017-05-25 18:38:23 +0300

    Test commit

--- BasicJavaExercises/src/com/zahariaca/arithmetic/Main.java
+++ BasicJavaExercises/src/com/zahariaca/arithmetic/Main.java
@@ -3,7 +3,7 @@ package com.zahariaca.arithmetic;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO  
+        //TODO  test
         Main main = new Main();
         main.checkNrOfArgs(args);
         main.checkIfNr(args);
