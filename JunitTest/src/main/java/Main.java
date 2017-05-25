1d3b33b14be95c2c693952e8d053c18d6c3748b0
zahariaca <zaharia.c.alexandru@gmail.com>
2017-05-25 10:57:12 +0300

    test

---
+++ JunitTest/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Zaharia Costin-Alexandru, email: zaharia.c.alexandru@gmail.com on 5/17/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JunitTest/src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Zaharia Costin-Alexandru, email: zaharia.c.alexandru@gmail.com on 5/17/2017.
+ */
+public class MainTest {
+
+    @Test
+    public void testTest(){
+        Assert.assertTrue(true);
+    }
+}
