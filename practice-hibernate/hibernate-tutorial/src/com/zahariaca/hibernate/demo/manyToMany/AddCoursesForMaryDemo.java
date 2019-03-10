df1a127d6320acdb1f8a019ecb49954df829afe0
Alexandru Zaharia <zaharia.c.alexandru@gmail.com>
2019-03-10 15:15:09 +0200

    Added many to many hibernate project

---
+++ practice-hibernate/hibernate-tutorial/src/com/zahariaca/hibernate/demo/manyToMany/AddCoursesForMaryDemo.java
@@ -0,0 +1,58 @@
+package com.zahariaca.hibernate.demo.manyToMany;
+
+import com.zahariaca.hibernate.demo.manyToMany.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author Zaharia Costin-Alexandru (zaharia.c.alexandru@gmail.com) on 10.03.2019
+ */
+public class AddCoursesForMaryDemo {
+    public static void main(String[] args) {
+        SessionFactory factory= new Configuration()
+                                    .configure("hibernate-instructor-manyToMany.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .addAnnotatedClass(Review.class)
+                                    .addAnnotatedClass(Student.class)
+                                    .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            // begin transaction
+            session.beginTransaction();
+
+            // get the student mary from the db
+            Student student = session.get(Student.class, 2);
+
+            System.out.println("Got student: " + student);
+            System.out.println("Got student with courses: " + student.getCourses());
+            // create more courses
+            Course course1 = new Course("Rubik's  Cube - How to Speed Cube");
+            Course course2 = new Course("Atari 2600 - Game Development");
+            Course course3  = new Course("Spring boot");
+
+            // add mary to those courses
+            System.out.println("Saving the courses...");
+            course1.addStudent(student);
+            course2.addStudent(student);
+            course3.addStudent(student);
+            System.out.println("Save finished");
+
+            // save
+            session.save(course1);
+            session.save(course2);
+            session.save(course3);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
---
+++ practice-hibernate/hibernate-tutorial/src/com/zahariaca/hibernate/demo/manyToMany/entity/InstructorDetail.java
@@ -0,0 +1,82 @@
+package com.zahariaca.hibernate.demo.manyToMany.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author Zaharia Costin-Alexandru (zaharia.c.alexandru@gmail.com) on 05.03.2019
+ */
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+    // annotate the fields with the db column names
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+    @Column(name = "hobby")
+    private String hobby;
+
+    @OneToOne(mappedBy = "instructorDetail",
+            cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    private Instructor instructor;
+
+    // create constructors
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getter/setter method
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    // generate toString() method
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
