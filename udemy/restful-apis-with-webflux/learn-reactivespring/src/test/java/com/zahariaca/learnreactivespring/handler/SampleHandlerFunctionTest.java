1f29dcae8b21f26f24f0621d05917c616977d161
zahariaca <zaharia.c.alexandru@gmail.com>
2021-08-08 13:09:21 +0300

    Adds example with Handler function and Router function + ITs

---
+++ udemy/restful-apis-with-webflux/learn-reactivespring/src/test/java/com/zahariaca/learnreactivespring/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,52 @@
+package com.zahariaca.learnreactivespring.handler;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class SampleHandlerFunctionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void flux_approach1() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void mono_approach1() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .verifyComplete();
+    }
+}
