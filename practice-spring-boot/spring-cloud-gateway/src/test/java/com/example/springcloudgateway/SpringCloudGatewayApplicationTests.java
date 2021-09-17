a43a8a0b6f3721021e88d8aefc9108bad62a861f
zahariaca <zaharia.c.alexandru@gmail.com>
2021-09-17 13:59:32 +0300

    Adds spring cloud gateway test project

---
+++ practice-spring-boot/spring-cloud-gateway/src/test/java/com/example/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,50 @@
+package com.example.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
+class SpringCloudGatewayApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void contextLoads() {
+        stubFor(get(urlEqualTo("/get"))
+                .willReturn(aResponse()
+                        .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                        .withHeader("Content-Type", "application/json")));
+
+        stubFor(get(urlEqualTo("/delay/3"))
+                .willReturn(aResponse()
+                        .withBody("no fallback")
+                        .withFixedDelay(3000)));
+
+        webTestClient
+                .get().uri("/get")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.headers.Hello").isEqualTo("World");
+
+        webTestClient
+                .get().uri("/delay/3")
+                .header("Host", "www.circuitbreaker.com")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes(StandardCharsets.UTF_8)));
+    }
+
+}
